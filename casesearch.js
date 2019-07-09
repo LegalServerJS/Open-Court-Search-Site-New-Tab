d73a627ef6abc36b40746e47cb5993e77be7621a
Matthew Stubenberg <matthewstubenberg@gmail.com>
2019-07-08 20:39:28 -0400

    Only one tab opens up
    Opens one tab then closes it and opens another one with the search results.

--- casesearch.js
+++ casesearch.js
@@ -1,97 +1,34 @@
-<a id='matthew_autoset' class="matthew_button" onclick="matthew_OpenTabWithSessionID();" style="cursor:pointer">Open Case Search</a>
+<a id='matthew_opencasesearch' class="matthew_button" onclick="matthewBillCaseSearch();" style="cursor:pointer">Open Case Search</a>
 <style type="text/css">
-    .matthew_button {
-        font: bold 11px Arial;
-        text-decoration: none;
-        background-color: #EEEEEE;
-        color: #333333;
-        padding: 2px 6px 2px 6px;
-        border-top: 1px solid #CCCCCC;
-        border-right: 1px solid #333333;
-        border-bottom: 1px solid #333333;
-        border-left: 1px solid #CCCCCC;
-    }  
+.matthew_button {
+    font: bold 11px Arial;
+    text-decoration: none;
+    background-color: #EEEEEE;
+    color: #333333;
+    padding: 2px 6px 2px 6px;
+    border-top: 1px solid #CCCCCC;
+    border-right: 1px solid #333333;
+    border-bottom: 1px solid #333333;
+    border-left: 1px solid #CCCCCC;
+}  
 </style>
 
 <script type="text/javascript">
-function matthew_OpenTabWithSessionID(){
-    //This function opens a new tab that accepts the terms and conditions of case search
-    //In turn case search will set the session cookie.
-    var url = 'http://casesearch.courts.state.md.us/casesearch/processDisclaimer.jis';
+
+function matthewBillCaseSearch(){
+    var baseURL = "http://casesearch.courts.state.md.us/casesearch/"
+    var processPath = 'processDisclaimer.jis?action=Continue&disclaimer=Y';
     
-    //Create fake form
-    var form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", url);
-    form.setAttribute("target", '_blank'); //Use _blank to open a new tab
+    //This is set to grab it from the intake page. If you are using this on the profile page you will have to change where the first name and last name.
+    firstname = jQuery("[name='matter:first']").val(); //Grab first name
+    lastname = jQuery("[name='matter:last']").val(); //Grab last name
     
-    //Equivalent of checking the box on case search
-    var input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = 'disclaimer';
-    input.value = 'Y';
-    form.appendChild(input);
-
-    //Equivalent of clicking Continue
-    var input2 = document.createElement('input');
-    input2.type = 'hidden';
-    input2.name = 'action';
-    input2.value = 'Continue';
-    form.appendChild(input2);
-
-    //Add form to website, submit, and then remove form.
-    document.body.appendChild(form);
-    form.submit();
-    document.body.removeChild(form);
-
-    //Wait 1 second just to make sure the session cookie is loaded
-    //Then load the next function which opens the search results tab
-    setTimeout(function(){
-        matthew_OpenTabWithSearchResults();
-    }, 1000);
-}
+    var namepath = "inquirySearch.jis?action=Search&company=N&courtSystem=B&firstName=" + firstname + "&lastName=" + lastname + "&site=00"
     
-function matthew_OpenTabWithSearchResults(){
-    var url = 'http://casesearch.courts.state.md.us/casesearch/inquirySearch.jis';
+    console.log("Opening Tab")
+    var win = window.open(baseURL + processPath); 
+    setTimeout(function() { win.close(); }, 300); //Wait 300 miliseconds then close this tab.
+    setTimeout(function(){ window.open(baseURL + namepath, '_blank') }, 700); //Wait 700 miliseconds then open the tab with the search results
 
-    /*
-    Grab the first and last name from the intake page
-    If you are doing this somewhere other than the intake page you will likely
-    need to modify the jquery search so it can find your fields. 
-    */
-    firstname = jQuery("[name='matter:first']").val();
-    lastname = jQuery("[name='matter:last']").val();
-
-    //Create new fake form
-    var form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", url);
-    form.setAttribute("target", '_blank');
-    
-    //Add last name to the form
-    var input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = 'lastName';
-    input.value = lastname;
-    form.appendChild(input);
-
-    //Add first name to the form
-    var input2 = document.createElement('input');
-    input2.type = 'hidden';
-    input2.name = 'firstName';
-    input2.value = firstname;
-    form.appendChild(input2);
-
-    //Add the the action parameter to the form to mimic what happens when you click on "search" on case search.
-    var input3 = document.createElement('input');
-    input3.type = 'hidden';
-    input3.name = 'action';
-    input3.value = 'Search';
-    form.appendChild(input3);
-
-    //Attach the form to the website, submit, and then remove
-    document.body.appendChild(form);
-    form.submit();
-    document.body.removeChild(form);
 }
 </script>
